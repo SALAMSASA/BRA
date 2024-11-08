deea1846d61fa99692f0cade6ef2d0e03a3989a6
SALAMSASA <kwkwkyz61@gmail.com>
2024-11-08 22:05:17 +0300

    Update play.py

--- BrandrdXMusic/plugins/play/play.py
+++ BrandrdXMusic/plugins/play/play.py
@@ -28,19 +28,19 @@ from config import BANNED_USERS, lyrical
 @app.on_message(filters.command(["تشغيل","شغل","فيد"], "")
 )
 @app.on_message(
-    filters.command(
-        [
-            "play",
-            "vplay",
-            "cplay",
-            "cvplay",
-            "playforce",
-            "vplayforce",
-            "cplayforce",
-            "cvplayforce",
-        ]
+    filters.command( 
+        [ 
+            "/play", 
+            "/vplay",
+            "cplay", 
+            "cvplay", 
+            "playforce", 
+            "vplayforce", 
+            "/cplayforce", 
+            "/cvplayforce", 
+        ],
+        ""
     )
-    & filters.group
     & ~BANNED_USERS
 )
 @PlayWrapper
